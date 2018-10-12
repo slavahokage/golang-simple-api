1442a15589ae706d163049b19cf576c9e9f3f683
slavahokage <slava123smirnov@gmail.com>
2018-10-12 10:02:52 +0300

    delete comments

--- application/main.go
+++ application/main.go
@@ -9,16 +9,6 @@ func main() {
 	router := gin.Default()
 	ac := controllers.СreateArticleController(router)
 	initializeRouting(&ac)
-	/*db := model.ConnectionToDB()
-	a := model.GetArticleModel()
-	db.AutoMigrate(a.GetArticle())
-	model.CreateNewArticle("test")
-	//ourAr := model.GetArticleById(*db,1)
-	//fmt.Println(ourAr)
-	test2 := a.GetArticle()
-	db.First(test2,1)
-	fmt.Println(test2.Text)
-	defer db.Close()*/
 }
 
 func initializeRouting(a *controllers.ArticleController) {
